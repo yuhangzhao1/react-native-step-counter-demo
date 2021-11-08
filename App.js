19a6deabc86c2adbd9f3a2a7caa78514d47750e5
yuhangzhao1 <75333593+yuhangzhao1@users.noreply.github.com>
2021-11-08 17:04:05 -0600

    create step counter

--- App.js
+++ App.js
@@ -1,21 +1,123 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Pedometer } from "expo-sensors";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    isPedometerAvailable: "checking",
+    pastStepCount: 0,
+    currentStepCount: 0,
+    buttonState: 'Reset Counter',
+    console: ''
+  };
+
+  _subscription = null;
+
+  componentDidMount() {
+    this._subscribe();
+  }
+
+  componentWillUnmount() {
+    this._unsubscribe();
+  }
+
+  _subscribe = () => {
+    this._subscription = Pedometer.watchStepCount(result => {
+      this.setState({
+        currentStepCount: result.steps
+      });
+    });
+
+    Pedometer.isAvailableAsync().then(
+      result => {
+        this.setState({
+          isPedometerAvailable: String(result)
+        });
+      },
+      error => {
+        this.setState({
+          isPedometerAvailable: "Could not get isPedometerAvailable: " + error
+        });
+      }
+    );
+
+    const end = new Date();
+    const start = new Date();
+    start.setDate(end.getDate() - 1);
+    Pedometer.getStepCountAsync(start, end).then(
+      result => {
+        this.setState({ pastStepCount: result.steps });
+      },
+      error => {
+        this.setState({
+          pastStepCount: "Could not get stepCount: " + error
+        });
+      }
+    );
+  };
+
+  _unsubscribe = () => {
+    this._subscription && this._subscription.remove();
+    this._subscription = null;
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.headline}>Total steps</Text>
+        <Text style={styles.valueName}>{this.state.pastStepCount}</Text>
+        <Text style={styles.headline}>Steps since count</Text>
+        <Text style={styles.valueName}>{this.state.currentStepCount}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  headline: {
+    fontSize: 24,
+    textAlign: 'center',
+    margin: 10,
+  },
+  valueContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  valueValue: {
+    width: 100,
+    fontSize: 20
+  },
+  valueName: {
+    fontSize: 48,
+    fontWeight: 'bold'
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    marginTop: 15,
+  },
+  button: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#d0d5d6',
+    padding: 10,
+    marginLeft: 100,
+    marginRight: 100,
+    borderRadius:10,
+  },
+  buttonText: {
+    fontSize: 20,
   },
 });
